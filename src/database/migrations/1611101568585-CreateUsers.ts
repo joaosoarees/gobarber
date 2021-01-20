ae312580d2c6f3f52e55e063798e4598f1ad6459
João Vitor <jsoareez@gmail.com>
2021-01-19 21:40:04 -0300

    update model

--- src/database/migrations/1611101568585-CreateUsers.ts
+++ src/database/migrations/1611101568585-CreateUsers.ts
@@ -28,12 +28,12 @@ export default class CreateUsers1611101568585 implements MigrationInterface {
           },
           {
             name: 'created_at',
-            type: 'timestamp with time zone',
+            type: 'timestamp',
             default: 'now()',
           },
           {
             name: 'updated_at',
-            type: 'timestamp with time zone',
+            type: 'timestamp',
             default: 'now()',
           },
         ],
