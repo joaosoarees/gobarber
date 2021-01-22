0d05597474c6c20ca5864aa2a7531a85da1a2e7c
João Vitor <jsoareez@gmail.com>
2021-01-21 21:06:09 -0300

    import multer and auth middleware

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 interface UserOptionalPassword {
   name: string;
@@ -9,6 +14,7 @@ interface UserOptionalPassword {
 }
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -29,5 +35,15 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
